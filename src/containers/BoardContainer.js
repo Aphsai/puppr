3d24314cf8f77eab610206d712747e348033d927
xcjackpan <xcjackpan99@gmail.com>
2018-02-21 22:59:26 -0500

    dimension changing working

--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -17,12 +17,31 @@ export default class BoardContainer extends React.Component {
           });
   }
 
+
+  // min width/height : 300px
+  // max width/height : 500px
+
+  // if height > 500, then multiply (500/height) onto width and vice versa for width
+
+  changeDimension = (height, width) => {
+    if (height > 500) {
+      width *= 500/height;
+      height = 500;
+    } else if (width > 500) {
+      height *= 500/width;
+      width = 500;
+    }
+    return {height:height, width:width}
+  }
+
   render() {
       console.log(this.state.gallery)
     return (
       <div>
         {this.state.gallery.map(data =>
-          <img style={{width: '300px', height: '300px'}} key={data.public_id} src={'http://res.cloudinary.com/dl2zhlvci/image/upload/v1519264049/' + data.public_id + '.jpg'}/>
+          
+            <img style={{width: this.changeDimension(data.height, data.width).width, height: this.changeDimension(data.height, data.width).height}} key={data.public_id} 
+            src={'http://res.cloudinary.com/dl2zhlvci/image/upload/v1519264049/' + data.public_id + '.jpg'}/>
         )}
       </div>
     );
