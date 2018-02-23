75fc60f9efacb52d7861b36781a6b4f47e57bd5e
aphsai <saksham.a@hotmail.ca>
2018-02-22 21:21:06 -0500

    database

---
+++ src/configs/db.js
@@ -0,0 +1,15 @@
+import { db } from './firebase';
+
+// User API
+
+export const doCreateUser = (id, username, email) =>
+  db.ref(`users/${id}`).set({
+    username,
+    email,
+  });
+
+export const onceGetUsers = () =>
+  db.ref('users').once('value');
+
+export const getSpecificUser = (uid) =>
+  db.ref('users/' + uid).once('value');
