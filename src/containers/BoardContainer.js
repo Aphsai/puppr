18617403e738c9986d9440798d483b7c2f7b85cb
aphsai <saksham.a@hotmail.ca>
2018-02-22 23:08:38 -0500

    basic

--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import { db } from '../configs';
+import ImageComponent from '../components/ImageComponent';
+
 
 export default class BoardContainer extends React.Component {
+
   constructor(props) {
       super(props);
       this.state = {
@@ -9,38 +13,40 @@ export default class BoardContainer extends React.Component {
       }
   }
 
- componentDidMount() {
-  axios.get('https://res.cloudinary.com/dl2zhlvci/image/list/dogs.json').then(res => {
+  componentDidMount() {
+    axios.get('https://res.cloudinary.com/dl2zhlvci/image/list/dogs.json').then(res => {
       console.log(res.data.resources);
       this.setState({gallery: res.data.resources});
     });
   }
-
-
-  // min width/height : 300px
-  // max width/height : 500px
-
-  // if height > 500, then multiply (500/height) onto width and vice versa for width
-
   changeDimension = (height, width) => {
-    if (height > 500) {
-      width *= 500/height;
-      height = 500;
-    } else if (width > 500) {
-      height *= 500/width;
-      width = 500;
+    if (height > width) {
+      width *= 300/height;
+      height = 300;
+    }
+    else {
+      height *= 300/width;
+      width = 300;
     }
     return {height:height, width:width}
   }
-
+  handleFavourite = (e) => {
+    let img_id = e.target.dataset.id;
+    console.log(img_id);
+  }
   render() {
       console.log(this.state.gallery)
     return (
       <div>
         {this.state.gallery.map(data =>
-          
-            <img style={{width: this.changeDimension(data.height, data.width).width, height: this.changeDimension(data.height, data.width).height}} key={data.public_id} 
-            src={'http://res.cloudinary.com/dl2zhlvci/image/upload/v1519264049/' + data.public_id + '.jpg'}/>
+          <ImageComponent
+            src={'http://res.cloudinary.com/dl2zhlvci/image/upload/v1519264049/' + data.public_id + '.jpg'}
+            dimension={this.changeDimension(data.height, data.width)}
+            key={data.public_id}
+            public_id={data.public_id}
+            handleFavourite={this.handleFavourite}
+            handleVote={this.handleVote}
+          />
         )}
       </div>
     );
--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -64,7 +64,6 @@ export default class HeaderContainer extends React.Component {
     });
   }
 
-
   render() {
     if (!this.state.authUser) {
       return (
