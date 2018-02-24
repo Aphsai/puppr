df3e9482fb9746a0d6fd884d84c681b077597896
aphsai <saksham.a@hotmail.ca>
2018-02-23 19:56:40 -0500

    added image container

---
+++ src/components/ImageComponent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class ImageComponent extends React.Component {
+
+
+
+
+  render() {
+    return (
+      <div>
+
+        <img width={this.props.dimension.width} height={this.props.dimension.height} src={this.props.src} />
+        <button
+          data-id={this.props.public_id}
+          data-width={this.props.dbDimension.width}
+          data-height={this.props.dbDimension.height}
+          onClick={this.props.handleFavourite}>
+            Heart
+        </button>
+        <button onClick={this.props.handleVote}> Upvote </button>
+
+      </div>
+    );
+  }
+}
